42dca0dc93e6622ae6dffe4a2baca600f6af6550
javaPhpMyAdmin <chelobat16411@gmail.com>
2021-10-28 17:39:38 -0300

    NavBar and index.tsx modified

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,19 +1,19 @@
-import React, {FunctionComponent, useEffect, useState} from 'react'
+import React, { FunctionComponent, useEffect, useState } from 'react'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 
 const NavItem: FunctionComponent<{
-    activeItem:string,
-    setActiveItem:Function,
-    name:string,
-    route:string
-}> = ({activeItem,setActiveItem,name,route}) => {
-    return(
+    activeItem: string,
+    setActiveItem: Function,
+    name: string,
+    route: string
+}> = ({ activeItem, setActiveItem, name, route }) => {
+    return (
         activeItem !== name ? (
             <Link href={route}>
                 <a><span onClick={() => setActiveItem(name)} className='hover:text-green'>{name}</span></a>
             </Link>
-        ):null
+        ) : null
     )
 }
 
@@ -22,11 +22,11 @@ function NavBar() {
 
     const { pathname } = useRouter()
 
-    useEffect(()=>{
+    useEffect(() => {
         if (pathname === '/') setActiveItem('About')
         if (pathname === '/projects') setActiveItem('Projects')
         if (pathname === '/resume') setActiveItem('Resume')
-    }, [])
+    }, [pathname])
 
     return (
         <div className='flex justify-between px-5 py-3 my-3'>
@@ -34,20 +34,20 @@ function NavBar() {
                 {activeItem}
             </span>
             <div className='flex space-x-5 text-lg '>
-                <NavItem 
-                    activeItem={activeItem} 
+                <NavItem
+                    activeItem={activeItem}
                     setActiveItem={setActiveItem}
                     name='About'
                     route='/'
                 />
-                <NavItem 
-                    activeItem={activeItem} 
+                <NavItem
+                    activeItem={activeItem}
                     setActiveItem={setActiveItem}
                     name='Resume'
                     route='/resume'
                 />
-                <NavItem 
-                    activeItem={activeItem} 
+                <NavItem
+                    activeItem={activeItem}
                     setActiveItem={setActiveItem}
                     name='Projects'
                     route='/projects'
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,41 +1,41 @@
 import { motion } from "framer-motion"
-import { fadeInUp, routerAnimation, stagger} from "../animations"
+import { fadeInUp, routerAnimation, stagger } from "../animations"
 import ServiceCard from "../components/ServiceCard"
 import { services } from "../data"
 
 const index = () => {
-  return(
-    <motion.div 
-      className='flex flex-col flex-grow px-6 pt-1' 
-      variants={routerAnimation} 
-      initial='initial' 
+  return (
+    <motion.div
+      className='flex flex-col flex-grow px-6 pt-1'
+      variants={routerAnimation}
+      initial='initial'
       animate='animate'
       exit='exit'
     >
       <h6 className='my-3 font-medium'>
-        I'm a software engineering student
+        I&aposm a software engineering student
         I am currently pursuing B. Tech Degree(Final Year) in Computer
-        Science Enginnering from University. I have 3+ years of 
+        Science Enginnering from University. I have 3+ years of
         experiene in Web Development and I have interesting on....bla bla blaa
       </h6>
-      <div 
-        className='flex-grow p-4 mt-5 bg-gray-400 dark:bg-dark-100' 
-        style={{marginRight:'-1.5rem',marginLeft:'-1.5rem'}}
+      <div
+        className='flex-grow p-4 mt-5 bg-gray-400 dark:bg-dark-100'
+        style={{ marginRight: '-1.5rem', marginLeft: '-1.5rem' }}
       >
         <h6 className='my-3 text-lg font-bold tracking-widest'>What I Offer</h6>
-        <motion.div 
+        <motion.div
           initial='initial'
           animate='animate'
           variants={stagger}
           className='grid gap-6 lg:grid-cols-2'>
           {
-            services.map( service => (
-              <motion.div 
+            services.map(service => (
+              <motion.div
                 variants={fadeInUp}
                 className='bg-gray-200 rounded-lg dark:bg-dark-200 lg:col-span-1'
                 key={service.about}
               >
-                <ServiceCard service={service}/>
+                <ServiceCard service={service} />
               </motion.div>
             ))
           }
